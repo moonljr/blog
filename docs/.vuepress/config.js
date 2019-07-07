fffd1b6405e5e2827155adec0ce4977f450ca22e
moonljr <13918718009@163.com>
2019-07-07 18:36:31 +0800

    blog init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: "Eru blog",
+  description: "我的个人网站",
+  // head: [ // 注入到当前页面的 HTML <head> 中的标签
+  // 	['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+  // ],
+  serviceWorker: false, // 是否开启 PWA
+  base: "/blog/", // 这是部署到github相关的配置
+  dest: "./blog",
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    nav: [
+      // 导航栏配置
+      { text: "主页", link: "/" },
+      { text: "入门", link: "/install/" },
+      { text: "入门2", link: "/demo/" },
+      { text: "关于", link: "/about/" },
+      { text: "GitHub", link: "https://github.com/" }
+    ],
+    // 侧边栏配置
+    sidebar: [
+      {
+        title: "入门",
+        children: ["/install/"]
+      },
+      {
+        title: "入门2",
+        children: ["/demo/"]
+      }
+    ],
+    sidebarDepth: 2 // 侧边栏显示2级
+  }
+};
---
+++ docs/demo/README.md
@@ -0,0 +1,12 @@
+---
+title: demo
+---
+
+# demo
+
+
+## demo1
+
+## demo2
+
+## demo3
